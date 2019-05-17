588ce7175bf39bb337428a3289edc678a185c1be
小码哥 <thisiswangle@gmail.com>
2019-05-17 18:55:08 +0800

    044 O(m+n)

---
+++ src/s004_median_of_two_sorted_arrays/Solution.java
@@ -0,0 +1,47 @@
+package s004_median_of_two_sorted_arrays;
+
+import java.util.Arrays;
+
+public class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+
+        // O(m+n)?
+        int length = nums1.length + nums2.length;
+        int[] num = new int[length];
+
+        int p1 = 0, p2 = 0, p = 0;
+
+        while (p < length) {
+            if (p1 < nums1.length && p2 < nums2.length) {
+                if (nums1[p1] < nums2[p2]) {
+                    num[p++] = nums1[p1++];
+                } else {
+                    num[p++] = nums2[p2++];
+                }
+            }
+
+            if (p1 < nums1.length && p2 >= nums2.length) {
+                num[p++] = nums1[p1++];
+            }
+
+            if (p1 >= nums1.length && p2 < nums2.length) {
+                num[p++] = nums2[p2++];
+            }
+        }
+
+        //System.out.println(Arrays.toString(num));
+
+        if (length % 2 == 0) {
+            return (num[length / 2] + num[length / 2 - 1]) / 2.0;
+        } else {
+            return num[length / 2] / 1.0;
+        }
+    }
+
+    public static void main(String[] args) {
+        double r = new Solution().findMedianSortedArrays(new int[]{1, 3}, new int[]{});
+        System.out.println(r);
+        r = new Solution().findMedianSortedArrays(new int[]{1, 2}, new int[]{3, 4});
+        System.out.println(r);
+    }
+}
