a6e302ddb5632394a3f8b57a9c9602cfb7d6a799
小码哥 <thisiswangle@gmail.com>
2019-05-17 17:28:38 +0800

    s036 s049

--- src/s036_valid_sudoku/Solution.java
+++ src/s036_valid_sudoku/Solution.java
@@ -1,4 +1,59 @@
 package s036_valid_sudoku;
 
 public class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        int size = 9;
+        int[] hash = null;
+        for (int i = 0; i < size; i ++) {
+            hash = new int[10];
+            for (int j = 0; j < size; j ++) {
+                if (board[i][j] == '.')
+                    continue;
+                else {
+                    int num = board[i][j] - '0';
+                    if (hash[num] == 0) {
+                        hash[num] = 1;
+                    } else {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < size; i ++) {
+            hash = new int[10];
+            for (int j = 0; j < size; j ++) {
+                if (board[j][i] == '.')
+                    continue;
+                else {
+                    int num = board[j][i] - '0';
+                    if (hash[num] == 0) {
+                        hash[num] = 1;
+                    } else {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < 3; i ++)
+            for (int j = 0; j < 3; j ++) {
+                hash = new int[10];
+                for (int k = i * 3; k < i * 3 + 3; k ++)
+                    for (int l = j * 3; l < j * 3 + 3; l ++) {
+                        if (board[k][l] == '.')
+                            continue;
+                        else {
+                            int num = board[k][l] - '0';
+                            if (hash[num] == 0) {
+                                hash[num] = 1;
+                            } else {
+                                return false;
+                            }
+                        }
+                    }
+            }
+
+        return true;
+    }
 }
--- src/s049_group_anagrams/Solution.java
+++ src/s049_group_anagrams/Solution.java
@@ -1,4 +1,44 @@
 package s049_group_anagrams;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Solution {
+    public List<String> anagrams(String[] strs) {
+        List<String> solution = new ArrayList<String>();
+        Map<String, List<String>> fpMap = new HashMap<String, List<String>>();
+        for (String str : strs) {
+            char[] chars = str.toCharArray();
+
+            // May be smaller
+            // int[] hash = new int['z' - 'a'];
+            int[] hash = new int[256];
+            for (int i = 0; i < chars.length; i ++) {
+                hash[chars[i]] = hash[chars[i]] + 1;
+            }
+
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = 0; i < hash.length; i ++) {
+                stringBuilder.append(hash[i]);
+            }
+            // Build a fingerprint
+            String fp = stringBuilder.toString();
+
+            List<String> s = fpMap.get(fp);
+            if (null == s) {
+                s = new ArrayList<String>();
+            }
+            s.add(str);
+            fpMap.put(fp, s);
+        }
+
+        for (Map.Entry<String, List<String>> entry : fpMap.entrySet()) {
+            if (entry.getValue().size() > 1) {
+                solution.addAll(entry.getValue());
+            }
+        }
+        return solution;
+    }
 }
