69ad70078e3454a46edacc1dc8dc81601d12e5fa
小码哥 <thisiswangle@gmail.com>
2019-05-17 18:03:29 +0800

    all code submit 5 years ago

--- readme.py
+++ readme.py
@@ -6,4 +6,4 @@ dirs = os.listdir(path)
 dirs.sort()
 for f in dirs:
     subdirs = os.listdir("%s/%s" % (path, f))
-    print("* [%s](blob/master/%s/%s/%s)" % (f, path, f, subdirs[0]))
+    print("* [%s](%s/%s/%s)" % (f, path, f, subdirs[0]))
