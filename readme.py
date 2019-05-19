29cd4a2b95458593446eb10f9a1a8c85252de38b
小码哥 <thisiswangle@gmail.com>
2019-05-19 09:03:58 +0800

    update readme

--- readme.py
+++ readme.py
@@ -3,8 +3,17 @@ import os
 path = 'src'
 dirs = os.listdir(path)
 
+def name(f):
+    words = f.split('_')
+    sentence = []
+    sentence.append('#' + words[0][1:])
+    for word in words[1:]:
+        #sentence.append(word[0].upper() + word[1:])
+        sentence.append(word.capitalize())
+    return " ".join(sentence)
+
 dirs.sort()
 print("# Solutions for Leetcode\n")
 for f in dirs:
     subdirs = os.listdir("%s/%s" % (path, f))
-    print("* [%s](%s/%s/%s)" % (f, path, f, subdirs[0]))
+    print("* [%s](%s/%s/%s)" % (name(f), path, f, subdirs[0]))
